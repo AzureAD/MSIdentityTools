237de480e190a5244b8e09ed4b4c6d364895e78b
Jason Thompson (ID CXP) <jasoth@microsoft.com>
2022-02-01 14:23:41 -0500

    Add warning on old versions of PowerShell

--- src/MSIdentityTools.psm1
+++ src/MSIdentityTools.psm1
@@ -1,12 +1,7 @@
 ﻿## Set Strict Mode for Module. https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-strictmode
 Set-StrictMode -Version 3.0
 
-## PowerShell Desktop 5.1 does not dot-source ScriptsToProcess when a specific version is specified on import. This is a bug.
-# if ($PSEdition -eq 'Desktop') {
-#     $ModuleManifest = Import-PowershellDataFile (Join-Path $PSScriptRoot $MyInvocation.MyCommand.Name.Replace('.psm1','.psd1'))
-#     if ($ModuleManifest.ContainsKey('ScriptsToProcess')) {
-#         foreach ($Path in $ModuleManifest.ScriptsToProcess) {
-#             . (Join-Path $PSScriptRoot $Path)
-#         }
-#     }
-# }
+## Display Warning on old PowerShell versions. https://docs.microsoft.com/en-us/powershell/scripting/install/PowerShell-Support-Lifecycle#powershell-end-of-support-dates
+if ($PSVersionTable.PSVersion -lt [version]'7.0') {
+    Write-Warning 'It is recommended to use this module with the latest version of PowerShell which can be downloaded here: https://aka.ms/install-powershell'
+}
