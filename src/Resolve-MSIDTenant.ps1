66445cf29147d89ae8820e3301ff08ef488b9daf
Jef Kazimer <jef@jeftek.com>
2022-02-03 17:28:04 -0600

    Added CODE-SAMPLE to Notes
    Added CODE-SAMPLE to notes

--- src/Resolve-MSIDTenant.ps1
+++ src/Resolve-MSIDTenant.ps1
@@ -14,9 +14,22 @@
     $DomainList = get-content .\DomainList.txt
     Resolve-MSIDTenant -Tenant $DomainList
 .NOTES
-    -  Azure AD OIDC Metadata endpoint - https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#fetch-the-openid-connect-metadata-document
+    - Azure AD OIDC Metadata endpoint - https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#fetch-the-openid-connect-metadata-document
     - A Result of NotFound does not mean that the tenant does not exist at all, but it might be in a different cloud environment.   Additional queries to other environments may result in it being found.
-
+    -     THIS CODE-SAMPLE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED 
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR 
+    FITNESS FOR A PARTICULAR PURPOSE.
+    This sample is not supported under any Microsoft standard support program or service. 
+    The script is provided AS IS without warranty of any kind. Microsoft further disclaims all
+    implied warranties including, without limitation, any implied warranties of merchantability
+    or of fitness for a particular purpose. The entire risk arising out of the use or performance
+    of the sample and documentation remains with you. In no event shall Microsoft, its authors,
+    or anyone else involved in the creation, production, or delivery of the script be liable for 
+    any damages whatsoever (including, without limitation, damages for loss of business profits, 
+    business interruption, loss of business information, or other pecuniary loss) arising out of 
+    the use of or inability to use the sample or documentation, even if Microsoft has been advised 
+    of the possibility of such damages, rising out of the use of or inability to use the sample script, 
+    even if Microsoft has been advised of the possibility of such damages.
 #>
 function Resolve-MSIDTenant {
     [CmdletBinding(DefaultParameterSetName = 'Parameter Set 1',
