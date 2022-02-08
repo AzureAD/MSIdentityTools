c9b9fa4f3b81ada6dce5dbc2990c7a37abea2bef
Jason Thompson (ID CXP) <jasoth@microsoft.com>
2022-02-07 21:47:31 -0500

    Workspace configuration and formatting

--- build/Launch-PSModule.ps1
+++ build/Launch-PSModule.ps1
@@ -1,13 +1,15 @@
-param
-(
+param (
     # Module to Launch
     [Parameter(Mandatory = $false)]
-    [string] $ModuleManifestPath = ".\src\*.psd1",
+    [string] $ModuleManifestPath = '.\src\*.psd1',
+    # ScriptBlock to Execute After Module Import
+    [Parameter(Mandatory = $false)]
+    [scriptblock] $PostImportScriptBlock,
     # Paths to PowerShell Executables
     [Parameter(Mandatory = $false)]
     [string[]] $PowerShellPaths = @(
-        #'pwsh'
-        'powershell'
+        'pwsh'
+        #'powershell'
         #'D:\Software\PowerShell-6.2.4-win-x64\pwsh.exe'
     ),
     # Import Module into the same session
@@ -19,15 +21,17 @@ param
 
 if ($NoNewWindow) {
     Import-Module $ModuleManifestPath -PassThru -Force
+    if ($PostImportScriptBlock) { Invoke-Command -ScriptBlock $PostImportScriptBlock -NoNewScope }
 }
 else {
     [scriptblock] $ScriptBlock = {
-        param ([string]$ModulePath)
+        param ([string]$ModulePath, [scriptblock]$PostImportScriptBlock)
         ## Force WindowsPowerShell to load correct version of built-in modules when launched from PowerShell 6+
         if ($PSVersionTable.PSEdition -eq 'Desktop') { Import-Module 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Utility', 'CimCmdlets' -MaximumVersion 5.9.9.9 }
         Import-Module $ModulePath -PassThru
+        Invoke-Command -ScriptBlock $PostImportScriptBlock -NoNewScope
     }
-    $strScriptBlock = 'Invoke-Command -ScriptBlock {{ {0} }} -ArgumentList {1}' -f $ScriptBlock, $ModuleManifestPath
+    $strScriptBlock = 'Invoke-Command -ScriptBlock {{ {0} }} -ArgumentList {1}, {{ {2} }}' -f $ScriptBlock, $ModuleManifestPath, $PostImportScriptBlock
     #$strScriptBlock = 'Import-Module {0} -PassThru' -f $ModuleManifestPath
 
     foreach ($Path in $PowerShellPaths) {
