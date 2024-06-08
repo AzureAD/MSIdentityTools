58aad1987bf12cdee7740c6424bec6c9032843e2
Merill Fernando <merill@merill.net>
2024-06-08 19:13:00 +1000

    Update Export-MsIdAzureMfaReport.ps1

--- src/Export-MsIdAzureMfaReport.ps1
+++ src/Export-MsIdAzureMfaReport.ps1
@@ -7,7 +7,9 @@
 
     ```powershell
     Install-Module MsIdentityTools -Scope CurrentUser
+
     Connect-MgGraph -Scopes Directory.Read.All, AuditLog.Read.All, UserAuthenticationMethod.Read.All
+
     Export-MsIdAzureMfaReport .\report.xlsx
     ```
 
