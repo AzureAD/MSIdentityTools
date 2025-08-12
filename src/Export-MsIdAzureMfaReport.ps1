a08caa6a4e83accde4790acf293758f32f91f61b
Nathan McNulty <6653432+nathanmcnulty@users.noreply.github.com>
2025-08-11 17:05:05 -0800

    Fix PropertyNotFoundException
    Fix PropertyNotFoundException: The property 'HasSignedInWithMfa' cannot be found on this object. Verify that the property exists.

--- src/Export-MsIdAzureMfaReport.ps1
+++ src/Export-MsIdAzureMfaReport.ps1
@@ -290,7 +290,7 @@ function Export-MsIdAzureMfaReport {
             AddMfaProperties $user
             UpdateProgress $currentCount $totalCount $user
 
-            if ($user.HasSignedInWithMfa) {
+            if ($null -ne $user.HasSignedInWithMfa) {
                 $user.MfaStatus = "MFA Capable + Signed in with MFA"
                 $user.MfaStatusIcon = "✅"
             }
